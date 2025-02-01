2936a9c19136afa860c28c10014ddae8f4d8218a
Prajeeth777 <prajeethkumar777@gmail.com>
2025-02-01 20:07:23 +0900

    Update activity 2.py

--- activity 2.py
+++ activity 2.py
@@ -1,1 +1,14 @@
+file1=open("WWW.txt","r")
+print("File in read mode\n")
+print(file1.read())
+file1.close()
 
+file2=open("WWW.txt","x")
+print("File in write mode....")
+file2.write("Hi I am Prajeeth Kumar and I am 15 years old")
+file2.close()
+
+file3=open("WWW.txt","x")
+print("File in append mode....")
+file3.write("\nI am Prajeeth Kumar and I am 15 years old")
+file3.close()
