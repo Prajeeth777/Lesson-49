45979b68cf9978517d5e740af8d0ee991a4895d7
Prajeeth777 <prajeethkumar777@gmail.com>
2025-02-01 20:32:58 +0900

    Update activity 4.py

--- activity 4.py
+++ activity 4.py
@@ -1,2 +1,22 @@
 firstfile=input("Enter the name of first file")
 secondfile=input("Enter the name of second file")
+
+f1=open(firstfile,'r')
+f2=open(secondfile,'r')
+
+print("File 1 before appending content \n",f1.read())
+print("File 2 before appending content \n",f2.read())
+
+f1.close()
+f2.close()
+
+f1=open(firstfile,'r')
+f2=open(secondfile,'a')
+
+f2.write(f1.read())
+
+f1.seek(0)
+f2.seek(0)
+
+f1.close()
+f2.close()
